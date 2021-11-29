a6a93139be5fd1facd42229aef54e38cddfc7dc9
lyamanek <luciano.oliveira@klab.com.br>
2021-11-29 07:38:52 -0300

    add abstract attribute component

---
+++ src/app/api/abstract.attribute.component.js
@@ -0,0 +1,14 @@
+class AbstractAttributeComponent {
+
+
+    constructor(attributesMap) {
+        this.attributesMap = attributesMap;
+    }
+
+    execute(entity) {
+        
+    }
+
+}
+
+module.exports.AbstractAttributeComponent = AbstractAttributeComponent;
